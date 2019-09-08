90ffa13f37dfeebfe2b7e97e3678fdcb6d3b7e3d
lovecoding530 <lovecoding530@gmail.com>
2019-09-08 08:37:58 -0700

    modify9-8

--- src/components/header.js
+++ src/components/header.js
@@ -29,13 +29,13 @@ export default class Header extends Component {
     render() {
       return(
         <View style={{...ifIphoneX({height: 80}, {height: 60}), width: responsiveWidth(100), backgroundColor: '#000', alignItems: 'center', flexDirection: 'row'}}>
-            <TouchableHighlight onPress={()=>{this.props.prop.goBack()}} style={{width: responsiveWidth(10), height:40, ...ifIphoneX({hmargintop: 40}, {marginTop: 20}), marginLeft: 5}}>
+            <TouchableHighlight onPress={()=>{this.props.prop.goBack()}} style={{width: responsiveWidth(10), height:40, ...ifIphoneX({marginTop: 40}, {marginTop: 20}), marginLeft: 5}}>
                 <Image source={Images.back} style={{width: 30, height: 30, marginTop: 5, tintColor: '#fff'}}></Image>
             </TouchableHighlight>
-            <TouchableHighlight onPress={()=>{this.props.prop.navigate('HomeScreen')}} style={{width: responsiveWidth(80), height: 40, ...ifIphoneX({hmargintop: 40}, {marginTop: 20}), alignItems: 'center'}}>
+            <TouchableHighlight onPress={()=>{this.props.prop.navigate('HomeScreen')}} style={{width: responsiveWidth(80), height: 40, ...ifIphoneX({marginTop: 40}, {marginTop: 20}), alignItems: 'center'}}>
                 <Image source={Images.home} style={{width: 30, height: 30, marginTop: 5, tintColor: '#fff'}}></Image>
             </TouchableHighlight>
-            <TouchableHighlight onPress={this.logout.bind(this)} style={{width: responsiveWidth(10), height: 40, marginTop: 20, marginRight: 5}}>
+            <TouchableHighlight onPress={this.logout.bind(this)} style={{width: responsiveWidth(10), height: 40, ...ifIphoneX({marginTop: 40}, {marginTop: 20}), marginRight: 5}}>
                 <Image source={Images.logout} style={{width: 30, height: 30, marginTop: 5, tintColor: '#fff'}}></Image>
             </TouchableHighlight>
         </View>
