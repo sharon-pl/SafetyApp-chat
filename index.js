959c230ea1f127c4f4a96a8c10e303501fffd8fd
WangFei <chengcai.jin@hotmail.com>
2019-11-11 12:12:32 +0800

    #Update

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+console.disableYellowBox = true
+
 AppRegistry.registerComponent(appName, () => App);
