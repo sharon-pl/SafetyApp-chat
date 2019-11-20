34d07b911c1414c70cdf70e7d7b1e4079ee2fcef
WangFei <chengcai.jin@hotmail.com>
2019-11-21 02:46:10 +0800

    #integrate OneSignal

--- src/screen/splash.js
+++ src/screen/splash.js
@@ -8,12 +8,34 @@ import { Images } from '../theme';
 import { Container } from 'native-base';
 import { responsiveWidth } from 'react-native-responsive-dimensions'
 import API from '../components/api'
+import OneSignal from 'react-native-onesignal'
 
 export default class splash extends Component {
     constructor(props) {
         super(props)
         
+        OneSignal.init("928954ff-21be-4995-a982-9cbd5ff295b2");
+        OneSignal.addEventListener("received", this.onReceived);
+        OneSignal.addEventListener("opened", this.onOpened);
+        OneSignal.addEventListener("ids", this.onIds);
+        OneSignal.configure();
     }
+
+    onReceived = notification => {
+        console.log("Notification received: ", notification);
+    };
+
+    onOpened = openResult => {
+        console.log("Message: ", openResult.notification.payload.body);
+        console.log("Data: ", openResult.notification.payload.additionalData);
+        console.log("isActive: ", openResult.notification.isAppInFocus);
+        console.log("openResult: ", openResult);
+    };
+
+    onIds = device => {
+        console.log("Device info: ", device);
+        this.setState({ device });
+    };
 
     async componentDidMount() {
       
