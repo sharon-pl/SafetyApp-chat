cc1c3a5599edd1a0863382a22583dc590b56c7b4
Wang <wang@atfold.com>
2020-04-16 23:50:45 -0700

    Insert module

--- android/app/src/main/java/com/safefyapp/MainApplication.java
+++ android/app/src/main/java/com/safefyapp/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.rumax.reactnative.pdfviewer.PDFViewPackage;
